51eeef25953efa35b718bbb4ff01ba07046edcf6
Alexander Sidorenko <sidorenko.alexander@gmail.com>
2015-06-09 21:17:32 -0700

    Refactored TI toolchain a little bit: moved into shared function code that searches for a piece of software only if it was not specified by user. This change simplified TI toolchain file, but will also help upcoming changes for GCC toolchain.

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -35,3 +35,29 @@ function(get_last_subdir PARENTDIR MASK OUT_SUBDIRS OUT_LAST_SUBDIR)
     set(${OUT_SUBDIRS} ${SUBDIRS} PARENT_SCOPE)
     set(${OUT_LAST_SUBDIR} ${LAST_SUBDIR} PARENT_SCOPE)
 endfunction(get_last_subdir)
+
+# If ${SOFTWARE_PATH} is not set, will search (non-recursively) by ${MASK} in
+# ${SEARCH_DIR} and return the last one in alphabetically sorted list.
+# If ${SOFTWARE_PATH} was set in the first place, it won't be modified. Function
+# will display messages (searching/found/not found) about it's progress using
+# ${SOFTWARE_NAME} as display name for the software.
+function(find_software_if_not_set SOFTWARE_PATH SOFTWARE_NAME SEARCH_DIR MASK)
+    if (NOT ${SOFTWARE_PATH})
+        message(STATUS "${SOFTWARE_PATH} is not set, looking for ${SOFTWARE_NAME} under \"${SEARCH_DIR}\"...")
+        get_last_subdir(${SEARCH_DIR} ${MASK} ALL_SOFTWARE_VERSION LATEST_SOFTWARE_VERSION)
+        set(${SOFTWARE_PATH} ${SEARCH_DIR}\\${LATEST_SOFTWARE_VERSION})
+        if (${SOFTWARE_PATH} AND EXISTS ${${SOFTWARE_PATH}})
+            # So far we have set local scope variable; now need to set parent scope
+            set(${SOFTWARE_PATH} ${${SOFTWARE_PATH}} PARENT_SCOPE)
+            message(STATUS "Auto-detected ${SOFTWARE_NAME} under \"${${SOFTWARE_PATH}}\". If you want to use different version of ${SOFTWARE_NAME}, please explicitly set ${SOFTWARE_PATH}.")
+        else()
+            message(FATAL_ERROR "Could not find ${SOFTWARE_NAME} under \"${SEARCH_DIR}\". Please install ${SOFTWARE_NAME} or set ${SOFTWARE_PATH}.")
+        endif()
+    else()
+        if (${SOFTWARE_PATH} AND EXISTS ${${SOFTWARE_PATH}})
+            message(STATUS "${SOFTWARE_PATH} is set, using ${SOFTWARE_NAME} installation under \"${${SOFTWARE_PATH}}\"")
+        else()
+            message(FATAL_ERROR "${SOFTWARE_PATH} is set, but \"${${SOFTWARE_PATH}}\" doesn't exist!")
+        endif()
+    endif()
+endfunction()
